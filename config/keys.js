647c892b4d8004dc191173f61e7619b45e51b093
Danial Kh <khoshkholgh.danial@gmail.com>
2018-11-13 04:55:58 -0500

    Change configs for deploying on server

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports={
     mongoURI:
-        'mongodb://khatere:khatere123@ds155833.mlab.com:55833/dev-connector',
+        'mongodb://localhost/dev-connector',
     secretOrKey:'secret'
 }
