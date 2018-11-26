9f507f042d9999344463644b5f7cc4658a116842
Danial Kh <khoshkholgh.danial@gmail.com>
2018-11-26 22:31:57 +0330

    Add pagination route
    Added route GET(/api/products) used for getting products per page size
    and number.

--- routes/api/products.js
+++ routes/api/products.js
@@ -23,7 +23,7 @@ const Product = require('../../models/Product');
 /**
  * Get product information
  *@route  {GET} /api/products/:product_id
- *@name   Get Product
+ *@name   Get Product_id
  */
 router.get('/:product_id', (req, res) => {
   Product.findById(req.params.product_id).then((product) => {
@@ -51,4 +51,60 @@ router.get('/:product_id', (req, res) => {
   })
 })
 
+/**
+ * Get paginated product information
+ *@route  {GET} /api/products
+ *@name   Get Products
+ *@queryparam {Number} [pagesize=10] Number of products to return
+ *@queryparam {Number} [page=1] Number of page
+ */
+router.get('', (req, res) => {
+  let pageSize = Number(req.query.pagesize);
+  let currentPage = Number(req.query.page);
+  const productQuery = Product.find();
+  let fetchedProducts = [];
+
+  //Default values
+  if (isNaN(pageSize)) {
+    pageSize = 10;
+  }
+  if (isNaN(currentPage)) {
+    currentPage = 1;
+  }
+  console.log(pageSize)
+  if (pageSize < 1 || pageSize > 100) {
+    return res.status(400).json({
+      message: "Invalid page size!"
+    })
+  }
+  if (currentPage < 1) {
+    return res.status(400).json({
+      message: "Invalid page request!"
+    })
+  }
+
+  productQuery.skip(pageSize * (currentPage - 1))
+    .limit(pageSize);
+
+  return productQuery
+    .then((documents) => {
+      fetchedProducts = documents;
+
+      return Product.count()
+    })
+    .then((count) => {
+      if (fetchedProducts.length > 0) {
+        res.status(200).json({
+          message: "Page fetched successfuly!",
+          maxProducts: count,
+          products: fetchedProducts
+        })
+      } else {
+        res.status(404).json({
+          message: "Products not found!"
+        });
+      }
+    });
+
+})
 module.exports = router;
