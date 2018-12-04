c59ef0b62ab68eba5ae3e4e6f2cf8e9f7f9996ab
Danial Kh <khoshkholgh.danial@gmail.com>
2018-12-04 14:32:11 -0500

    Check NODE_ENV variable for db connection in production/dev environment

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,11 @@
-ApiKey = 'AvfwWKB5H3247yTAhPaA';
+let URI;
+if (process.env.NODE_ENV === "PRODUCTION"){
+   URI = 'mongodb://expressApp:AvfwWKB5H3247yTAhPaA@localhost' +
+     ':31000/kdk-shop?authSource=admin'
+}else{
+   URI = "mongodb://127.0.0.1:27017/kdk"
+}
 module.exports = {
-  // mongoURI: 'mongodb://expressApp:'+ ApiKey+ '@95.216.119.91' +
-    // ':31000/kdk-shop?authSource=admin',
- mongoURI: "mongodb://127.0.0.1:27017/kdk",
+   mongoURI: URI,
   secretOrKey: 'dieDUQvA5S9qReKIj0TSlpNbrnfYSePF'
 }
