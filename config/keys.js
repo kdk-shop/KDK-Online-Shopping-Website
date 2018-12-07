7a2bf49eb8688f7beb3c41cd01678594e6a103d8
Danial Kh <khoshkholgh.danial@gmail.com>
2018-12-07 21:10:08 +0330

    Fix db connection for server

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,11 @@
 let URI;
-// if (process.env.NODE_ENV === "PRODUCTION"){
-//    URI = 'mongodb://expressApp:AvfwWKB5H3247yTAhPaA@95.216.119.91' +
-//      ':31000/kdk-shop?authSource=admin'
-// }else{
+if (process.env.NODE_ENV === "PRODUCTION"){
+  URI = 'mongodb://expressApp:AvfwWKB5H3247yTAhPaA@95.216.119.91' +
+    ':31000/kdk-shop?authSource=admin'
+ }else{
    URI = "mongodb://127.0.0.1:27017/kdk"
-// }
+ }
 module.exports = {
-   mongoURI: URI,
+  mongoURI: URI,
   secretOrKey: 'dieDUQvA5S9qReKIj0TSlpNbrnfYSePF'
 }
