543a37184fd57853273dfc6089d2bb7518f187f7
Danial Kh <khoshkholgh.danial@gmail.com>
2018-12-08 19:22:35 +0330

    Add morgan for logging on dev environment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const childProcess = require('child_process');
+const morgan = require('morgan');
 
 const db = require('./db.js');
 const users = require('./routes/api/users');
@@ -10,6 +11,11 @@ const products = require('./routes/api/products');
 
 const app = express();
 
+//log using morgan
+if (process.env.NODE_ENV !== "PRODUCTION"){
+  app.use(morgan('dev'));
+}
+//Set CORS headers
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -31,11 +37,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+//run deply script and return response for github
 function deploy (res) {
   childProcess.exec('/home/dark0ne/deploy.sh', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
-
     if (err) {
       console.error(err);
       return res.send(500);
@@ -44,6 +50,7 @@ function deploy (res) {
   });
 }
 
+//Router for git webhook autodeployment on tag pushes starting with 'v'
 app.post("/webhooks/github", (req, res) => {
   if (process.env.NODE_ENV === "PRODUCTION" &&
     (/^.*tags\/v[0-9.]*$/).test(req.body.ref)) {
