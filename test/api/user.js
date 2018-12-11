bd8ca5d952f803526e87dc2ea566a99edb0bd1ad
Danial Kh <khoshkholgh.danial@gmail.com>
2018-12-11 23:50:40 +0330

    Change user tests
    User tests are now independent.

--- test/api/user.js
+++ test/api/user.js
@@ -12,31 +12,42 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 describe('Users', () => {
-  before((done) => {
+  //Make sure users collection is empty except for test user before each test
+
+  beforeEach((done) => {
+    User.remove({}, (err) => {
+      let testUser = new User({
+        name: "test",
+        email: "test@example.com",
+        password: "$2a$10$U2axE8DVGi2m/BAt4RDFZeMG" +
+          "t0OPSkRf8T0oec5KFVIBy5Y4fGBUa",
+        address: "Test address",
+        phoneNumber: 989120000000
+      });
+
+      testUser.save((err) => {
+        done();
+      })
+    });
+  });
+  //Again make sure users collection is empty after tests
+  afterEach((done) => {
     User.remove({}, (err) => {
       done();
     });
   });
 
   /*
-   *after((done) => {
-   * User.remove({}, (err) => {
-   * done();
-   * });
-   * });
-   */
-  /*
    * Register test user
    */
-  describe('Valid user authentication and profile management requests', () => {
-    let jwt;
+  describe('User registrarion and authentication', () => {
 
-    it('it should register user', (done) => {
+    it('it should register new user', (done) => {
       chai.request(server)
         .post('/api/users/register')
         .send({
-          "name": "Test",
-          "email": "test@example.com",
+          "name": "Test2",
+          "email": "test2@example.com",
           "password": "123456",
           "password2": "123456"
         })
@@ -44,8 +55,8 @@ describe('Users', () => {
           res.should.have.status(201);
           res.should.be.json;
           res.body.should.be.a('object');
-          res.body.user.should.have.property("name", "Test");
-          res.body.user.should.have.property("email", "test@example.com");
+          res.body.user.should.have.property("name", "Test2");
+          res.body.user.should.have.property("email", "test2@example.com");
           res.body.user.should.have.property("password");
           res.body.user.should.have.property("date");
           done();
@@ -64,11 +75,153 @@ describe('Users', () => {
           res.should.be.json;
           res.body.should.be.a('object');
           res.body.should.have.property("token");
-          jwt = res.body.token;
           done();
         });
     });
 
+    it('it should not register user with' +
+      ' empty email/name/password/confirm fields', (done) => {
+        chai.request(server)
+          .post('/api/users/register')
+          .send({})
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            jwt = res.body.token;
+            res.body.should.have.property("email", "Email field is required");
+            res.body.should.have.property(
+              "password",
+              "Password field is required"
+            );
+            res.body.should.have.property(
+              "password2",
+              "Confirm password is required"
+            );
+            res.body.should.have.property("name", "Name field is required");
+            done();
+          });
+      });
+
+    it('it should not register user with' +
+      ' invalid email/short name/short pwd', (done) => {
+        chai.request(server)
+          .post('/api/users/register')
+          .send({
+            email: "asdcwqo.asa",
+            name: "a",
+            password: "12"
+          })
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property("email", "Email is invalid");
+            res.body.should.have.property(
+              "password",
+              "Password must be at least 6 characters"
+            );
+            res.body.should.have.property(
+              "name",
+              "Name must be between 2 and 30 characters"
+            );
+            done();
+          });
+      });
+
+    it('it should not register user with' +
+      ' incorrect confirm password', (done) => {
+        chai.request(server)
+          .post('/api/users/register')
+          .send({
+            email: "test@example.com",
+            name: "test",
+            password: "123456",
+            password2: "123446"
+          })
+          .end((err, res) => {
+            res.should.have.status(400);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property(
+              "password2",
+              "Passwords must match"
+            );
+            done();
+          });
+      });
+
+    it('it should not register existing user', (done) => {
+      chai.request(server)
+        .post('/api/users/register')
+        .send({
+          "name": "test",
+          "email": "test@example.com",
+          "password": "123456",
+          "password2": "123456"
+        })
+        .end((err, res) => {
+          res.should.have.status(409);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property("name", "Username already exists");
+          res.body.should.have.property("email", "Email already exists");
+          done();
+        });
+    });
+
+    it('it should not login user with empty fields', (done) => {
+      chai.request(server)
+        .post('/api/users/login')
+        .send({})
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property(
+            "email",
+            "Email field is required"
+          );
+          res.body.should.have.property(
+            "password",
+            "Password field is required"
+          );
+          done();
+        });
+    });
+
+    it('it should not login user with incorrect credintials', (done) => {
+      chai.request(server)
+        .post('/api/users/login')
+        .send({
+          email: "test@example.com",
+          password: "492123"
+        })
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property("password", "Password incorrect");
+          done();
+        });
+    });
+  });
+
+  describe('User profile', () => {
+    let jwt;
+
+    beforeEach((done) => {
+      chai.request(server)
+        .post('/api/users/login')
+        .send({
+          email: "test@example.com",
+          password: "123456"
+        })
+        .end((err, res) => {
+          jwt = res.body.token;
+          done();
+        });
+    });
     it('it should update user profile', (done) => {
       chai.request(server)
         .post('/api/users/profile')
@@ -94,8 +247,8 @@ describe('Users', () => {
           res.should.have.status(200);
           res.should.be.json;
           res.body.should.be.a('object');
-          res.body.should.have.property("name", "Test2");
-          res.body.should.have.property("email", "test2@example.com");
+          res.body.should.have.property("name", "test");
+          res.body.should.have.property("email", "test@example.com");
           res.body.should.have.property("address", "Test address");
           res.body.should.have.property("tel", 989120000000);
           done();
@@ -107,7 +260,7 @@ describe('Users', () => {
         .post('/api/users/change_pwd')
         .set("Authorization", "Bearer " + jwt)
         .send({
-          oldPassword: "123456",
+          currentPassword: "123456",
           password: "654321",
           password2: "654321"
         })
@@ -121,143 +274,52 @@ describe('Users', () => {
       chai.request(server)
         .patch('/api/users/reset_pwd')
         .send({
-          email: "test2@example.com"
+          email: "test@example.com"
         })
         .end((err, res) => {
           res.should.have.status(200);
           done();
         });
     });
-  });
 
-  describe('Invalid user authentication and' +
-    ' profile management requests', () => {
-      let jwt;
-
-      it('it should not register user with' +
-        ' empty email/name/password/confirm fields', (done) => {
-          chai.request(server)
-            .post('/api/users/register')
-            .send({})
-            .end((err, res) => {
-              res.should.have.status(400);
-              res.should.be.json;
-              res.body.should.be.a('object');
-              res.body.should.have.property("email", "Email field is required");
-              res.body.should.have.property(
-                "password",
-                "Password field is required"
-              );
-              res.body.should.have.property(
-                "password2",
-                "Confirm password is required"
-              );
-              res.body.should.have.property("name", "Name field is required");
-              done();
-            });
+    it('it should not change user password without jwt', (done) => {
+      chai.request(server)
+        .post('/api/users/change_pwd')
+        .send({
+          currentpassword: "123456",
+          password: "492123",
+          password2: "492123"
+        })
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
         });
-
-      it('it should not register user with' +
-        ' invalid email/short name/short pwd', (done) => {
-          chai.request(server)
-            .post('/api/users/register')
-            .send({
-              email: "asdcwqo.asa",
-              name: "a",
-              password: "12"
-            })
-            .end((err, res) => {
-              res.should.have.status(400);
-              res.should.be.json;
-              res.body.should.be.a('object');
-              res.body.should.have.property("email", "Email is invalid");
-              res.body.should.have.property(
-                "password",
-                "Password must be at least 6 characters"
-              );
-              res.body.should.have.property(
-                "name",
-                "Name must be between 2 and 30 characters"
-              );
-              done();
-            });
-        });
-
-      it('it should not register user with' +
-        ' incorrect confirm password', (done) => {
-          chai.request(server)
-            .post('/api/users/register')
-            .send({
-              email: "test@example.com",
-              name: "test",
-              password: "123456",
-              password2: "123446"
-            })
-            .end((err, res) => {
-              res.should.have.status(400);
-              res.should.be.json;
-              res.body.should.be.a('object');
-              res.body.should.have.property(
-                "password2",
-                "Passwords must match"
-              );
-              done();
-            });
-        });
-
-      it('it should not register existing user', (done) => {
-        chai.request(server)
-          .post('/api/users/register')
-          .send({
-            "name": "Test2",
-            "email": "test2@example.com",
-            "password": "123456",
-            "password2": "123456"
-          })
-          .end((err, res) => {
-            res.should.have.status(409);
-            res.should.be.json;
-            res.body.should.be.a('object');
-            res.body.should.have.property("name", "Username already exists");
-            res.body.should.have.property("email", "Email already exists");
-            done();
-          });
-      });
-
-      it('it should not login user with empty fields', (done) => {
-        chai.request(server)
-          .post('/api/users/login')
-          .send({})
-          .end((err, res) => {
-            res.should.have.status(400);
-            res.should.be.json;
-            res.body.should.be.a('object');
-            res.body.should.have.property(
-              "email",
-              "Email field is required"
-            );
-            res.body.should.have.property(
-              "password",
-              "Password field is required"
-            );
-            done();
-          });
-      });
-
-      it('it should not login user with incorrect credintials', (done) => {
-        chai.request(server)
-          .post('/api/users/login')
-          .send({
-            email: "test2@example.com",
-            password: "492123"
-          })
-          .end((err, res) => {
-            res.should.have.status(401);
-            res.should.be.json;
-            res.body.should.be.a('object');
-            res.body.should.have.property("password", "Password incorrect");
-            done();
-          });
-      });
     });
+
+    it('it should not change user password with empty fields', (done) => {
+      chai.request(server)
+        .post('/api/users/change_pwd')
+        .set("Authorization", "Bearer " + jwt)
+        .send({})
+        .end((err, res) => {
+          console.log(res.body)
+          res.should.have.status(400);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          res.body.should.have.property(
+            "password",
+            "New password is required"
+          );
+          res.body.should.have.property(
+            "password2",
+            "Confirm password is required"
+          );
+          res.body.should.have.property(
+            "currentPassword",
+            "Current password is required"
+          );
+          done();
+        });
+    });
+  });
 });
