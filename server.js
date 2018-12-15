88dc10ec25692bc8770a4fd3340d3038f5afa082
Danial Kh <khoshkholgh.danial@gmail.com>
2018-12-16 02:08:27 +0330

    Add server make static directory

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const app=require('./app');
+const fs = require('fs');
+
 const port =process.env.PORT || 4950;
+
+//Setup statics folder for multer
+if (!fs.existsSync('static')) {
+    fs.mkdirSync('static');
+    fs.mkdirSync('static/images');
+    fs.mkdirSync('static/images/full');
+}
 
 app.listen(port,()=>console.log(`server running on port  ${port}`));
 
